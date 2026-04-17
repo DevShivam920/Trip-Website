59eb977b23a735e58fd028b180890503b3e14f55
Shivam Singh <dellindia920@gmail.com>
2026-04-17 06:05:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector(".menu-btn");
+  menuBtn.addEventListener("click", function () {
+ 
+    window.location.href = "explore.html";
+
+  });
+});
